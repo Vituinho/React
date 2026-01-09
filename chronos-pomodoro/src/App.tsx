e89d43094f5878b6bad5329be006fb256f6c0e62
Vituinho <trabalhovictor531@gmail.com>
2026-01-09 09:28:59 -0300

    feat: Criando componente de menu com links e ícones

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -4,6 +4,7 @@ import './styles/global.css';
 import { Container } from './components/Container';
 import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 export function App() {
   return (
@@ -16,7 +17,7 @@ export function App() {
 
       <Container>
          <Heading>
-            MENU
+            <Menu/>
          </Heading>
       </Container>
      </>
