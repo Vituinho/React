af6008a19013bb6b5d4617c7b4168c7aa7ff7d67
Vituinho <trabalhovictor531@gmail.com>
2026-01-11 01:00:32 -0300

    feat: Criando o componente Cycles com indicadores do ciclo pomodoro e cores

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -7,46 +7,51 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
 
 export function App() {
   return (
-     <>
-      <Container> 
-         <Heading>
-            <Logo/>
-         </Heading>
+    <>
+      <Container>
+        <Heading>
+          <Logo />
+        </Heading>
       </Container>
 
       <Container>
-         <Heading>
-            <Menu/>
-         </Heading>
+        <Heading>
+          <Menu />
+        </Heading>
       </Container>
 
       <Container>
-         <CountDown/>
+        <CountDown />
       </Container>
 
       <Container>
-         <form className='form' action="">
-            <div className="formRow">
-               <DefaultInput placeholder='Digite algo' labelText='task' id="meuInput" type="text"/>
-            </div>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              placeholder='Digite algo'
+              labelText='task'
+              id='meuInput'
+              type='text'
+            />
+          </div>
 
-            <div className="formRow">
-               <p>Lorem ipsum dolor sit amet.</p>
-            </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
 
-            <div className="formRow">
-               <p>Ciclos</p>
-               <p>0 0 0 0 0 0 0</p>
-            </div>
+          <div className='formRow'>
+            <Cycles/>
+          </div>
 
-            <div className="formRow">
-               <button>Enviar</button>
-            </div>
-         </form>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
-     </>
+    </>
   );
 }
