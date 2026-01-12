3331f6d322ecdceb601bdb70c152907a1443efc7
Vituinho <trabalhovictor531@gmail.com>
2026-01-12 11:44:01 -0300

    feat: Criando a página Home e um template para todas as outras páginas

---
+++ chronos-pomodoro/src/pages/NotFound/index.tsx
@@ -0,0 +1,15 @@
+import { Container } from "../../components/Container";
+import { MainTemplate } from "../../templates/MainTemplate";
+
+export function NotFound() {
+    return (
+        <Container>
+            <MainTemplate>
+                <h1>Página não encontrada</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod dolorem impedit magni velit labore quas neque earum, inventore nemo eligendi! Cum blanditiis odit nisi, natus in veritatis sequi quod quidem.
+                </p>
+            </MainTemplate>
+        </Container>
+    );
+}
