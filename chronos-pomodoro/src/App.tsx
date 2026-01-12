ef37f1d97e13586e2849a32067d04f27c7d7f400
Vituinho <trabalhovictor531@gmail.com>
2026-01-12 10:04:25 -0300

    feat: Usando useState para o estado com programação reativa

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -11,23 +11,29 @@ import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
+import { useState } from 'react';
 
 export function App() {
-  let numero = 0;
+  // Que todos os componentes que usam "numero"
+  // saibam das mudanças em seu valor.
+
+  // Sempre que eu usar useState, não vou usar atribuição diretamente
+  /* const [numero, setNumero] = useState(() => {
+    console.log('lazy initialization');
+    return 0;
+  }); */
+  const [numero, setNumero] = useState(0);
 
   function handleClick() {
-    const span = document.getElementById('numero');
-
-    if(!span) return;
-
-    numero++;
-    span.innerText = numero.toString();
-    console.log(numero, Date.now());
+    /* 
+    setNumero(prevState => prevState + 1); // Assim se copiar mais ele faz mais
+    //setNumero(numero + 1); */
+    setNumero(1);
   }
 
   return (
     <>
-      <Heading>Número: <span id="numero">{numero}</span></Heading>
+      <Heading>Número: {numero}</Heading>
       <button onClick={handleClick}>Aumenta</button>
 
       <Container>
