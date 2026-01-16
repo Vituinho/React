0451260a7d46f10d775088f34045b8322d99d640
Vituinho <trabalhovictor531@gmail.com>
2026-01-16 02:54:02 -0300

    feat: Em qual componente devo colocar o estado da aplicação no React?

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,8 +1,9 @@
-import './styles/theme.css';
-import './styles/global.css';
 import { AboutPomodoro } from './pages/AboutPomodoro';
 import { NotFound } from './pages/NotFound';
 import { Home } from './pages/Home';
+
+import './styles/theme.css';
+import './styles/global.css';
 
 export function App() {
   return <Home/>;
