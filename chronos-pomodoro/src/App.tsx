452bba93f935073620bf3705c9dfa1fa01b07795
Vituinho <trabalhovictor531@gmail.com>
2026-01-16 03:07:06 -0300

    feat: Estado global da aplicação com useState e comunicação via props

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,10 +1,28 @@
 import { AboutPomodoro } from './pages/AboutPomodoro';
 import { NotFound } from './pages/NotFound';
 import { Home } from './pages/Home';
+import type { TaskStateModel } from './models/TaskStateModel';
+
+import { useState } from 'react';
 
 import './styles/theme.css';
 import './styles/global.css';
 
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
+
 export function App() {
-  return <Home/>;
+  const [state, setState] = useState(initialState);
+  
+  return <Home state={state} setState={setState} />;
 }
