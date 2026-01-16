6044b8da9bd59d64c11fe257538a90ee945adb13
Vituinho <trabalhovictor531@gmail.com>
2026-01-16 10:04:22 -0300

    feat: Capturando o evento submit do formulário (onSubmit com preventDefault)

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -4,28 +4,34 @@ import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 
 export function MainForm() {
-    return (
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              placeholder='Digite algo'
-              labelText='task'
-              id='meuInput'
-              type='text'
-            />
-          </div>
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-          <div className='formRow'>
-            <p>Próximo intervalo é de 25min</p>
-          </div>
+    console.log('Deu Certo!');
+  }
 
-          <div className='formRow'>
-            <Cycles/>
-          </div>
+  return (
+      <form onSubmit={handleCreateNewTask} className='form' action=''>
+        <div className='formRow'>
+          <DefaultInput
+            placeholder='Digite algo'
+            labelText='task'
+            id='meuInput'
+            type='text'
+          />
+        </div>
 
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-    );
+        <div className='formRow'>
+          <p>Próximo intervalo é de 25min</p>
+        </div>
+
+        <div className='formRow'>
+          <Cycles/>
+        </div>
+
+        <div className='formRow'>
+          <DefaultButton icon={<PlayCircleIcon />} />
+        </div>
+      </form>
+  );
 }   
