72459c83deb4c1a48f07de48b0d1a6e734c44cdf
Vituinho <trabalhovictor531@gmail.com>
2026-01-21 14:39:33 -0300

    feat: React Toastify com adapter - mensagens reutilizáveis do jeito certo

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -3,11 +3,14 @@ import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider'
 
 import './styles/theme.css';
 import './styles/global.css';
+import { Bounce, ToastContainer } from 'react-toastify';
+import { MessagesContainer } from './components/MessagesContainer';
 
 export function App() {
   return (
     <TaskContextProvider>
       <Home/>
+      <MessagesContainer/>
     </TaskContextProvider>
   );
 }
