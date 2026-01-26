c369a3feaf17012bbefdb90f6e8aa0e3140c36cb
Vituinho <trabalhovictor531@gmail.com>
2026-01-26 15:48:21 -0300

    feat: Ordenando o array de tarefas na interface + função sortTasks

--- chronos-pomodoro/src/pages/History/index.tsx
+++ chronos-pomodoro/src/pages/History/index.tsx
@@ -8,9 +8,11 @@ import styles from './styles.module.css'
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { formatDate } from "../../utils/formatDate";
 import { getTaskStatus } from "../../utils/getTaskStatus";
+import { sortTasks } from "../../utils/sortTasks";
 
 export function History() {
-  const {state, dispatch} = useTaskContext();
+  const {state} = useTaskContext();
+  const sortedTasks = sortTasks({ tasks: state.tasks });
 
   return (
     <>
@@ -43,7 +45,7 @@ export function History() {
                 </thead>
 
                 <tbody>
-                  {state.tasks.map(task => {
+                  {sortedTasks.map(task => {
                     const taskTypeDictionary = {
                       workTime: 'Foco',
                       shortBreakTime: 'Descanço curto',
